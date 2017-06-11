66876be7d16ca96a950d0a9878dc00aaa1b41843
yfu <yfu@dyn-129-236-226-63.dyn.columbia.edu>
2017-06-11 10:59:10 -0400

    commit msg

---
+++ HelloWorld/src/com/rbccm/cmit/HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.rbccm.cmit.HelloWorld;
+
+/**
+ * @author yfu
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ask Leo! ~:)");
+		
+
+	}
+
+}
